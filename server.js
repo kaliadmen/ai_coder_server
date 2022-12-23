76d0de598ad1eec0e18ce7bca5635e61f4c9c4e2
kaliadmen <kaliadmen@gmail.com>
2022-12-23 15:39:54 -0500

    REMOVE console  log for debugging

--- server.js
+++ server.js
@@ -40,7 +40,6 @@ app.post('/', async (req, res) => {
     });
 
   } catch (error) {
-    console.log(process.env.OPENAI_KEY);
     console.error(error)
     res.status(500).send(error || 'Something went wrong');
   }
